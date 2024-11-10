cb7e1bb9ed7bc16690b9474028b9cb4c71f22664
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-10 15:13:19 +0700

    feat(public): add sitemap, pre-setup, and web config

--- public/index.php
+++ public/index.php
@@ -4,14 +4,25 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Define the base path for the application, so that we can use it in the future (e.g. for deployment)
+$BASE_PATH = __DIR__.'/..';
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $BASE_PATH.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+// Check if environment is missing
+if (!file_exists($BASE_PATH.'/.env')) {
+    if (!strpos($_SERVER['REQUEST_URI'], 'pre-setup') !== false) {
+        header('Location: /pre-setup.html');
+        exit;
+    }
+}
+
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $BASE_PATH.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $BASE_PATH.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
