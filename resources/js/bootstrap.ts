f9b1229445cc8ede45e04395b56849f13ca2b7da
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-10 15:13:39 +0700

    feat(resource): remove old resources

--- resources/js/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['X-InvFest-RequestHash'] =
-    import.meta.env.VITE_APP_NAME.split('')
-        .sort(() => Math.random() - 0.5)
-        .join('') || '';
