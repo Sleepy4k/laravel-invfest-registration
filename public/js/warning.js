1e570a2a883286293f6666fa8ee8951c80b4699f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-11 01:03:30 +0700

    feat(public): add old resource assets

---
+++ public/js/warning.js
@@ -0,0 +1,17 @@
+console.log(
+    '%cHold Up!',
+    'color: #5955B2; font-size: 50px; font-weight: bold; text-shadow: 2px 2px black;',
+);
+console.log(
+    'Jika seseorang menyuruh Anda untuk menyalin/menempel sesuatu di sini, Anda memiliki peluang 11/10 bahwa Anda sedang ditipu',
+);
+console.log(
+    '%cMenempelkan apa pun di sini dapat memberi penyerang akses ke akun Anda.',
+    'color: #EF0103; font-size: 15px; font-weight: bold;',
+);
+console.log(
+    'Kecuali Anda benar-benar paham apa yang Anda lakukan, tutup jendela ini dan tetaplah aman.',
+);
+console.log(
+    'Jika Anda benar-benar mengerti apa yang Anda lakukan, Anda harus bekerja sama dengan kami',
+);
