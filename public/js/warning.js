957917a11c8cabb75a8e39ee39bb00080823a500
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-12 12:01:12 +0700

    chore(public): update warning script for better reability

--- public/js/warning.js
+++ public/js/warning.js
@@ -1,17 +1,24 @@
-console.log(
-    '%cHold Up!',
-    'color: #5955B2; font-size: 50px; font-weight: bold; text-shadow: 2px 2px black;',
-);
-console.log(
-    'Jika seseorang menyuruh Anda untuk menyalin/menempel sesuatu di sini, Anda memiliki peluang 11/10 bahwa Anda sedang ditipu',
-);
-console.log(
-    '%cMenempelkan apa pun di sini dapat memberi penyerang akses ke akun Anda.',
-    'color: #EF0103; font-size: 15px; font-weight: bold;',
-);
-console.log(
-    'Kecuali Anda benar-benar paham apa yang Anda lakukan, tutup jendela ini dan tetaplah aman.',
-);
-console.log(
-    'Jika Anda benar-benar mengerti apa yang Anda lakukan, Anda harus bekerja sama dengan kami',
-);
+const messages = [
+    {
+        message: '%cHold Up!',
+        style: 'color: #5955B2; font-size: 50px; font-weight: bold; text-shadow: 2px 2px black;'
+    },
+    {
+        message: 'Jika seseorang menyuruh Anda untuk menyalin/menempel sesuatu di sini, Anda memiliki peluang 11/10 bahwa Anda sedang ditipu',
+        style: ''
+    },
+    {
+        message: '%cMenempelkan apa pun di sini dapat memberi penyerang akses ke akun Anda.',
+        style: 'color: #EF0103; font-size: 15px; font-weight: bold;'
+    },
+    {
+        message: 'Kecuali Anda benar-benar paham apa yang Anda lakukan, tutup halaman ini dan tetaplah aman.',
+        style: ''
+    },
+    {
+        message: 'Jika Anda benar-benar mengerti apa yang Anda lakukan, Anda harus bekerja sama dengan kami',
+        style: ''
+    }
+];
+
+messages.forEach(data => console.log(data.message, data.style));
