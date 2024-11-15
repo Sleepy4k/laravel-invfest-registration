3377cdf5b893e92a076b72fd1f9e1a8060407b87
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-15 23:32:38 +0700

    chore(bootstrap): add email verify middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'verification_email_access' => \App\Http\Middleware\VerificationEmailAccess::class,
         ]);
 
         $middleware->redirectGuestsTo('/');
