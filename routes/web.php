b59b6d4784e7a2bf4846413d0e573c37a4e43bc1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-15 23:30:06 +0700

    chore(route): add fixed web route

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,7 @@ use App\Http\Controllers\Team;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Frontend\LandingController::class, 'index'])->name('frontend.landing');
-Route::get('/competition/list', [Frontend\CompetitionController::class, 'index'])->name('frontend.competition.index');
 Route::get('/competition/{slug}', [Frontend\CompetitionController::class, 'show'])->name('frontend.competition.show');
-
-Route::get('/email/verify', [Auth\VerificationController::class, 'index'])->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}', [Auth\VerificationController::class, 'show'])->name('verification.verify');
-Route::post('/email/verification-check', [Auth\VerificationController::class, 'store'])->name('verification.send');
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [Auth\LoginController::class, 'index'])->name('login');
@@ -20,29 +15,39 @@ Route::middleware('guest')->group(function () {
 
     Route::get('/register', [Auth\RegisterController::class, 'index'])->name('register');
     Route::post('/register', [Auth\RegisterController::class, 'store'])->name('register.store');
+
+    Route::get('/competition/list', [Frontend\CompetitionController::class, 'index'])->name('frontend.competition.index');
 });
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', Auth\LogoutController::class)->name('logout');
 
     Route::middleware('role:team')->group(function () {
-        Route::get('/team-members', [Auth\TeamController::class, 'index'])->name('team-members');
-        Route::post('/team-members', [Auth\TeamController::class, 'store'])->name('team-members.store');
+        Route::middleware('verification_email_access')->group(function () {
+            Route::get('/email/verify', [Auth\VerificationController::class, 'index'])->name('verification.notice');
+            Route::get('/email/verify/{id}/{hash}', [Auth\VerificationController::class, 'show'])->name('verification.verify');
+            Route::post('/email/verification-check', [Auth\VerificationController::class, 'store'])->name('verification.send');
+        });
 
-        Route::get('/payment-team', [Auth\PaymentController::class, 'index'])->name('payment-team');
-        Route::post('/payment-team', [Auth\PaymentController::class, 'store'])->name('payment-team.store');
+        Route::middleware('verified')->group(function () {
+            Route::get('/team-members', [Auth\TeamController::class, 'index'])->name('team-members');
+            Route::post('/team-members', [Auth\TeamController::class, 'store'])->name('team-members.store');
 
-        Route::middleware('verified')->prefix('team')->name('team.')->group(function () {
-            Route::get('/dashboard', Team\DashboardController::class)->name('dashboard');
-            Route::get('/karya', [Team\SubmissionController::class, 'index'])->name('work');
-            Route::post('/karya', [Team\SubmissionController::class, 'store'])->name('work.store');
+            Route::get('/payment-team', [Auth\PaymentController::class, 'index'])->name('payment-team');
+            Route::post('/payment-team', [Auth\PaymentController::class, 'store'])->name('payment-team.store');
+
+            Route::prefix('team')->name('team.')->group(function () {
+                Route::get('/dashboard', Team\DashboardController::class)->name('dashboard');
+                Route::get('/karya', [Team\SubmissionController::class, 'index'])->name('work');
+                Route::post('/karya', [Team\SubmissionController::class, 'store'])->name('work.store');
+            });
         });
     });
 
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::middleware('role:petugas|admin')->group(function () {
             Route::get('/dashboard', Admin\DashboardController::class)->name('dashboard');
-            Route::resource('team', Admin\TeamController::class);
+            Route::resource('team', Admin\TeamController::class)->except(['create', 'store', 'edit']);
         });
 
         Route::middleware('role:admin')->group(function () {
@@ -50,11 +55,11 @@ Route::middleware('auth')->group(function () {
             Route::put('/website-configuration/{id}', [Admin\TeamController::class, 'update'])->name('website-configuration.update');
 
             Route::resource('competition', Admin\TeamController::class);
-            Route::resource('timeline', Admin\TeamController::class);
-            Route::resource('payment-method', Admin\TeamController::class);
-            Route::resource('sponsor', Admin\TeamController::class);
-            Route::resource('media-partner', Admin\TeamController::class);
-            Route::resource('work', Admin\TeamController::class);
+            Route::resource('timeline', Admin\TimelineController::class);
+            Route::resource('payment-method', Admin\PaymentMethodController::class)->except('show');
+            Route::resource('sponsor', Admin\SponsorshipController::class)->except('show');
+            Route::resource('media-partner', Admin\MediaPartnerController::class)->except('show');
+            Route::resource('work', Admin\SubmissionController::class)->only(['index', 'update']);
         });
     });
 });
