2bb7e5d3a810550331cceb36eec5652a9a4a5a90
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-11-18 21:00:18 +0700

    chore(config): update some config for better performance

--- config/cache.php
+++ config/cache.php
@@ -44,12 +44,14 @@ return [
             'table' => env('DB_CACHE_TABLE', 'cache'),
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
             'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+            'events' => false,
         ],
 
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
             'lock_path' => storage_path('framework/cache/data'),
+            'events' => false,
         ],
 
         'memcached' => [
