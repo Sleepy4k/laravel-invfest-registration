4b65540c35ca55a8485a3fd4a9362ff7d489276c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-12-10 00:01:32 +0700

    chore(resource): update some page

--- resources/views/components/frontend/card/sponsor.blade.php
+++ resources/views/components/frontend/card/sponsor.blade.php
@@ -1,18 +1,21 @@
-<div class="container card-container-custom">
-    <div class="d-flex flex-center">
-        @foreach ($sponsors as $sponsor)
-            <div
-                data-aos="fade-up"
-                data-aos-delay="{{ ($loop->iteration ^ 2) * 50 }}"
-                class="card card-custom"
-            >
-                <img
-                    src="{{ $sponsor->logo ?? '#' }}"
-                    class="card-img card-img-custom"
-                    alt="{{ $sponsor->name }}"
-                    loading="lazy"
-                />
-            </div>
-        @endforeach
+@foreach ($sponsorsTiers as $tier)
+    <div class="container card-container-custom">
+        <h2 class="text-white text-center">{{ Str::ucfirst($tier->tier) }}</h2>
+        <div class="d-flex flex-center mt-3 items-center">
+            @foreach ($tier->sponsorship as $sponsor)
+                <div
+                    data-aos="fade-up"
+                    data-aos-delay="{{ ($loop->iteration ^ 2) * 50 }}"
+                    class="card card-custom"
+                >
+                    <img
+                        src="{{ $sponsor->logo ?? '#' }}"
+                        class="card-img card-img-custom"
+                        alt="{{ $sponsor->name }}"
+                        loading="lazy"
+                    />
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
+@endforeach
